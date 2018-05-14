0f581c9e9c24dd9674198afbd140a61d16f5e171
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-05-14 11:37:42 +0200

    also fix missing ' here

--- Scripts/testDevice2.bash
+++ Scripts/testDevice2.bash
@@ -1,4 +1,4 @@
 while true
 do
-	xcodebuild -project ../Control/RDRaidMapCtrl.xcodeproj -scheme "RDRaidMapCtrl" -destination 'platform=iOS,name=Device 2 Name‚ test
+	xcodebuild -project ../Control/RDRaidMapCtrl.xcodeproj -scheme "RDRaidMapCtrl" -destination 'platform=iOS,name=Device 2 Name'‚ test
 done
