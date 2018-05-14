50b6a066a1eef19dff15ec07baf543e89408468e
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-05-14 11:37:25 +0200

    fix missing '

--- Scripts/testDevice1.bash
+++ Scripts/testDevice1.bash
@@ -1,4 +1,4 @@
 while true
 do
-	xcodebuild -project ../Control/RDRaidMapCtrl.xcodeproj -scheme "RDRaidMapCtrl" -destination 'platform=iOS,name=Device 1 Name‚ test
+	xcodebuild -project ../Control/RDRaidMapCtrl.xcodeproj -scheme "RDRaidMapCtrl" -destination 'platform=iOS,name=Device 1 Name'‚ test
 done
